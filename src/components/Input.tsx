eb048895a38bdc5b8861a645a1ab0d97037c64da
Maros Tomasov <maros3845@gmail.com>
2024-02-08 14:22:14 +0100

    Styling: Input.css + adding usestate values to input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './input.css';
 
 function Input() {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(value); // Print the value to the console
+    };
+
     return (
-        <form className="form">
+        <>
+        <form className="form" onSubmit={handleSubmit}>
             <h2>Name of your city </h2>
             <div className='form_control'>
-                <input type="text" className="form_input" />
-                <button className="form_button">Add</button>
+                <input type="text" className="form_input" onChange={(e) => setValue(e.target.value)}  placeholder='Enter your city'/>
+                <button className="form_button" type="submit">Add</button>
             </div>
         </form>
-    )
+        <p>{value}</p>
+        </>
+    );
 }
+        
 
 export default Input;
