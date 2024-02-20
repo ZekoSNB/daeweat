8cb13fe2e3c8e95fa980a56d26881863337eac97
Maros Tomasov <maros3845@gmail.com>
2024-02-20 14:57:33 +0100

    unknown changes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import './input.css';
 
 function Input() {
+    let show = false;
+    let count = 0;
     const [value, setValue] = useState('');
 
     const handleSubmit = (e: React.FormEvent) => {
+        if (count % 2 === 0 ) {
+            show = true;
+        }
+        else {
+            show = false;
+        }
+        count++;
         e.preventDefault();
+        console.log(show);
         console.log(value); // Print the value to the console
     };
 
@@ -18,7 +28,8 @@ function Input() {
                 <button className="form_button" type="submit">Add</button>
             </div>
         </form>
-        <p>{value}</p>
+        {show ? <div>{value}</div> : null}
+        {value}
         </>
     );
 }
